06b1d9cedbb119c57ca82192fd0a19eb3280288c
ubdussamad <ubdussamad@gmail.com>
2019-02-06 22:48:08 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Bismillah-he ar-Rehman ar-Rahim
+# EMS - Custom
+# Ref: 6Feb2018
+# Version: 0
+# Author: Mohammesd S. Haque <ubdussamad@gmail.com>
+# Contract For: Nouman
+# Copyright 2019 EMD Systems.
+
+import maths
+
+
+
+
+def main ( *args , **kwargs ) :
+    # Will be using some C-style Control for ease in debugging
+    return(0)
+}

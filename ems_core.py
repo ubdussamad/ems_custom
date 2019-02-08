be80b4c00788d80cb7fd500f0daa1e981c45365b
ubdussamad <ubdussamad@gmail.com>
2019-02-08 15:07:11 +0530

    Some layout detailing

--- ems_core.py
+++ ems_core.py
@@ -3,26 +3,29 @@
 # IMM ( Inventory Management Module)
 # LMM ( Log / Hisotry Management Module)
 # CMM ( Client Management Module)
-
+import datetime
 
 class imm ( object ):
 	def __init__ ( self , id):
 		self.i_id = id # Inventory Id 
 		self.i_name = None # Inventory Name
-		self.data = None
+		self.data = None #This is a list of lists
 	
-	#Routine for loading data into class object defined by the inventory id
-	def load ( self ):
+	#Routine for loading and parsing data into class object
+        #defined by the inventory id
+	def parse_log ( self ):
 		try:
-			self.data = load_i(self.i_id)
-			return(0)
+			with open('ivn.log','r') as f_obj:
+                                self.log = f_obj.read()
+                                self.log = self.log.split('\n')
+                                self.log = [i.split('||') for i in self.log]
 		except:
 			return("0x1")
 
 	def update ( self ):
 		self.load(self)
 
-	def search ( self  , key):
+	def search ( self , key):
 		# Will be done after planing data format
 		return
 
